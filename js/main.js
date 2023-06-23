9645849b4e89b1bd1fe91a620494fac0e75b83ee
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2023-06-23 16:42:09 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,123 @@
+// // menu start
+// var menu = document.getElementById("menu");
+// var menuBtn = document.getElementById("menuBtn");
+// var body = document.body;
+// menuBtn.onclick = function () {
+//   menu.classList.toggle("active");
+//   menuBtn.classList.toggle("active");
+//   body.classList.toggle("active");
+// };
+// window.onclick = function (event) {
+//   if (event.target == menu) {
+//     menu.classList.remove("active");
+//     menuBtn.classList.remove("active");
+//     body.classList.remove("active");
+//   }
+// };
+// // menu end
+
+// // scroll start
+// let header = document.getElementById("header");
+// function scrollFunc() {
+//   if (window.pageYOffset >= 60) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+// const links = document.querySelectorAll(".links");
+// const sections = document.querySelectorAll(".anchor");
+// function changeLinkState() {
+//   let index = sections.length;
+//   while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
+//   links.forEach((link) => link.classList.remove("active"));
+//   links[index]?.classList.add("active");
+// }
+// links.forEach((e) => {
+//   onLinkClick(e);
+// });
+// function onLinkClick(linkItem) {
+//   linkItem.addEventListener("click", function () {
+//     menu.classList.remove("active");
+//     menuBtn.classList.remove("active");
+//     body.classList.remove("active");
+//   });
+// }
+// window.onscroll = function () {
+//   changeLinkState();
+//   scrollFunc();
+// };
+// scroll end
+// // faq start
+// const tabBtn = document.querySelectorAll(".tabBtn");
+// const tabEvent = document.querySelectorAll(".tabEvent");
+// tabBtn.forEach((e) => {
+//   onTabClick(tabBtn, tabEvent, e);
+// });
+// function onTabClick(tabBtns, tabItems, item) {
+//   item.addEventListener("click", function (e) {
+//     let currentBtn = item;
+//     let tabId = currentBtn.getAttribute("data-tab");
+//     let currentTab = document.querySelector(tabId);
+//     if (currentBtn.classList.contains("active")) {
+//       console.log("now active");
+//       const faq = currentBtn.parentElement.querySelector(".tabEvent");
+//       if (faq) {
+//         faq.classList.remove("active");
+//         currentBtn.classList.remove("active");
+//       }
+//     } else if (!currentBtn.classList.contains("active")) {
+//       tabBtns.forEach(function (item) {
+//         item.classList.remove("active");
+//       });
+
+//       tabItems.forEach(function (item) {
+//         item.classList.remove("active");
+//       });
+//       currentBtn.classList.add("active");
+//       currentTab.classList.add("active");
+//     }
+//   });
+// }
+// // faq end
+// modals start
+// const closeBtns = document.querySelectorAll(".modallClose").forEach((e) => {
+//   e.addEventListener("click", function (x) {
+//     console.log(x.target.dataset.modal);
+//     var ModalId = x.target.dataset.modal;
+//     document.querySelector("#" + ModalId).classList.remove("active");
+//   });
+// });
+// const modallBtn = document.querySelectorAll(".modallBtn");
+// const modallItems = document.querySelectorAll(".modall");
+// modallBtn.forEach((e) => {
+//   onTabClick(modallBtn, modallItems, e);
+// });
+// function onTabClick(modallBtns, modallItems, item) {
+//   item.addEventListener("click", function (e) {
+//     let currentBtn = item;
+//     let tabId = currentBtn.getAttribute("data-tab");
+//     let currentTab = document.querySelector(tabId);
+//     if (e.srcElement.classList.contains("active")) {
+//       e.srcElement.classList.remove("active");
+//     } else if (!currentBtn.classList.contains("active")) {
+//       modallBtns.forEach(function (item) {
+//         item.classList.remove("active");
+//       });
+//       modallItems.forEach(function (item) {
+//         item.classList.remove("active");
+//       });
+//       //currentBtn.classList.add('active');
+//       currentTab.classList.add("active");
+//     }
+//   });
+//   window.onclick = function (event) {
+//     modallItems.forEach((black) => {
+//       if (black == event.target) {
+//         black.classList.remove("active");
+//       }
+//     });
+//   };
+// }
+// // modals end
+
